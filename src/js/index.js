4a7a139ce829a8b14d9cae684f64e07ef02406d4
Swapneel Mehta <swapneel.mehta96@gmail.com>
2017-07-22 01:57:09 +0200

    added chat ui - unformatted

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Layout from "./pages/layout";
+import Chats from "./components/chatbox/chats";
 
 const app = document.getElementById('app');
 
--- src/js/pages/layout.js
+++ src/js/pages/layout.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Link } from 'react-router';
 
 // import component here
-import Sidebar from '../components/sidebar/sidebar'
+import Sidebar from '../components/sidebar/sidebar';
+import Chats from '../components/chatbox/chats';
 
 export default class Layout extends React.Component {
+	
 
     render() {
         return (
             <div id="main-app">
                 <Sidebar/>
+                <Chats/>
             </div>
         );
     }
 }
+
+
