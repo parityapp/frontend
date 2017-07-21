a72a1090a570e6e9fbc4b5bea23ebf332daf7277
Pierpaolo <plucarelli.94@gmail.com>
2017-07-22 00:43:09 +0100

    dummy data with Recharts

--- src/js/components/activity/activity.js
+++ src/js/components/activity/activity.js
@@ -1,15 +1,15 @@
 // imports
 import React from 'react';
-import { YAxis, XAxis, CartesianGrid, LineChart, Line } from 'recharts';
+import { YAxis, XAxis, CartesianGrid, AreaChart, Tooltip, Area } from 'recharts';
 
 const data = [
-      {name: '09:00', uv: 4000, pv: 2400, amt: 2400},
-      {name: '09:30', uv: 3000, pv: 1398, amt: 2210},
-      {name: '10:00', uv: 2000, pv: 9800, amt: 2290},
-      {name: '10:30', uv: 2780, pv: 3908, amt: 2000},
-      {name: '11:00', uv: 1890, pv: 4800, amt: 2181},
-      {name: '11:30', uv: 2390, pv: 3800, amt: 2500},
-      {name: '12:00', uv: 3490, pv: 4300, amt: 2100},
+      {name: '09:00', uv: 4000},
+      {name: '09:30', uv: 3000},
+      {name: '10:00', uv: 2000},
+      {name: '10:30', uv: 2780},
+      {name: '11:00', uv: 1890},
+      {name: '11:30', uv: 2390},
+      {name: '12:00', uv: 9000},
 ];
 
 // nav component
@@ -26,12 +26,14 @@ export default class Activity extends React.Component{
 
         return(
             <div style={style} className="sidebar">
-                <LineChart width={600} height={300} data={data}>
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                </LineChart>
+            <AreaChart width={1000} height={400} data={data}
+                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Area type='monotone' fillOpacity={1} dataKey='uv' stroke='#8884d8' fill='#6164c1' />
+            </AreaChart>
             </div>
         );
     }
