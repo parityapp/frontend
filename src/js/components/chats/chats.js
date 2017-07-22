049610e2119782141a1098976ec04518762c3856
Pierpaolo <plucarelli.94@gmail.com>
2017-07-22 21:01:31 +0100

    added chats

--- src/js/components/chats/chats.js
+++ src/js/components/chats/chats.js
@@ -10,6 +10,15 @@ export default class Chats extends React.Component {
             },
             {
                 text: "this is a message also"
+            },
+            {
+                text: "this is a message also"
+            },
+            {
+                text: "this is a message also"
+            },
+            {
+                text: "this is a message also"
             }
         ]
     }
@@ -19,6 +28,7 @@ export default class Chats extends React.Component {
     return (
         <div className="container app-container">
             <div className="chat">
+                <h1>5 important messages</h1>
                 {this.state.messages.map(function(msg, index){
                     return <div key={index} className="chatbox">
                                 <p>{msg.text}</p>
